b6f37de8ed1c6b5aa4dc3bb292077c1433c680bc
Pablo Caldentey <pablo@caldentey.org>
2020-06-13 03:47:26 +0200

    Dictionary with product database added. Now with a real dictionary

---
+++ app/src/db/db.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+CATALOG = {
+        'PEN': {'description': 'Lana Pen', 'price': 500, "discounts": None},
+        'TSHIRT': {'description': 'Lana T-Shirt', 'price': 2000, "discounts": None},
+        'MUG': {'description': 'Lana Coffee Mug', 'price': 750, "discounts": None}
+}
