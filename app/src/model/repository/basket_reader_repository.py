9bf8639934afad6e389bed29a8f996a2592693f9
Pablo Caldentey <pablo@caldentey.org>
2020-06-13 07:01:26 +0200

    basket_reader_repository created

---
+++ app/src/model/repository/basket_reader_repository.py
@@ -0,0 +1,38 @@
+import pickle
+
+from abc import ABC
+from abc import abstractmethod
+from pathlib import Path
+
+from src.model.basket import Basket
+from src.exceptions import BasketFileNotFoundException
+
+
+class BasketReaderRepository(ABC):
+
+    @abstractmethod
+    def get(
+            self,
+            basket_id: int,
+            database: str,
+    ) -> Basket:
+        raise NotImplementedError()
+
+
+class FileBasketReaderRepository(BasketReaderRepository):
+
+    def get(
+            self,
+            basket_id: int,
+            database: str,
+    ) -> Basket:
+        file_path = Path(database + str(basket_id))
+        if file_path.is_file():
+            with open(file_path, 'rb') as basket_file:
+                content = pickle.load(basket_file)
+                basket = Basket()
+                basket.id = basket_id
+                basket.products = content
+                return basket
+        else:
+            raise BasketFileNotFoundException("Basket file {} not found".format(basket_id))
