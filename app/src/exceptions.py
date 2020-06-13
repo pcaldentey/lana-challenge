160fa0c442c0384dfd9e6763003f7dee4d54fb50
Pablo Caldentey <pablo@caldentey.org>
2020-06-13 03:36:14 +0200

    Product reader repository interface and Dictionary product reader repository class

--- app/src/exceptions.py
+++ app/src/exceptions.py
@@ -1,3 +1,8 @@
 class BasketFileNotFoundException(Exception):
     """User defined exception. Basket not found"""
     pass
+
+
+class ProductNotFoundException(Exception):
+    """User defined exception. Product not found"""
+    pass
---
+++ app/src/model/repository/product_reader_repository.py
@@ -0,0 +1,39 @@
+from abc import ABC
+from abc import abstractmethod
+
+from src.db.db import CATALOG
+from src.exceptions import ProductNotFoundException
+from src.model.product import Product
+
+
+class ProductReaderRepository(ABC):
+
+    @abstractmethod
+    def get(
+            self,
+            product_code: str,
+            database: str = None,
+    ) -> Product:
+        raise NotImplementedError()
+
+
+class DictionaryProductReaderRepository(ProductReaderRepository):
+
+    def get(
+            self,
+            product_code: str,
+            database: str = None,
+    ) -> Product:
+
+        # Read product from CATALOG dictionary
+        # 'PEN': {'description': 'Lana Pen', 'price': 500, 'currency': '€', 'discounts': None},
+        if product_code in CATALOG:
+            product = Product()
+            product.id = product_code
+            product.description = CATALOG[product_code]['description']
+            product.price_per_unit = CATALOG[product_code]['description']
+            product.discount_to_apply = CATALOG[product_code]['discounts']
+            return product
+
+        else:
+            raise ProductNotFoundException("Product with code: {} not found".format(product_code))
