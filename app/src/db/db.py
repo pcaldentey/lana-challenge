98f9781ea93110eafb0042e583509ec8c3de4a8f
Pablo Caldentey <pablo@caldentey.org>
2020-06-13 10:28:55 +0200

    Discounts information added into database DISCOUNTS dictionary

--- app/src/db/db.py
+++ app/src/db/db.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 CATALOG = {
-        'PEN': {'description': 'Lana Pen', 'price': 500, "discounts": None},
-        'TSHIRT': {'description': 'Lana T-Shirt', 'price': 2000, "discounts": None},
-        'MUG': {'description': 'Lana Coffee Mug', 'price': 750, "discounts": None}
+        'PEN': {'description': 'Lana Pen', 'price': 500, 'currency': '€', 'discounts': ['2per1']},
+        'TSHIRT': {'description': 'Lana T-Shirt', 'price': 2000, 'currency': '€', 'discounts': ['bulk25']},
+        'MUG': {'description': 'Lana Coffee Mug', 'price': 750, 'currency': '€', 'discounts': None}
+}
+
+DISCOUNTS = {
+        '2per1': {'description': '2 por 1', 'status': 'active'},
+        'bulk25': {
+            'description': 'Bulk discount 25%',
+            'status': 'active',
+            'percent': 25,
+            'limit_items': 3,
+            'discount_type': 'BULK'
+        }
 }
