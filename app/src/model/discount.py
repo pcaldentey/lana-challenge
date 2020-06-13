1095f767dcff751dba574bee20feef37b72317ea
Pablo Caldentey <pablo@caldentey.org>
2020-06-13 13:16:48 +0200

    Discount class, discount strategies and Discount reader repository added

---
+++ app/src/model/discount.py
@@ -0,0 +1,57 @@
+from abc import ABC
+from abc import abstractmethod
+
+
+class DiscountStrategy(ABC):
+    """ Discount Strategy Interface class """
+
+    @abstractmethod
+    def apply_discount(self, items_number, price_per_item):
+        pass
+
+
+class Discount:
+    """ Discount placeholder class """
+
+    def __init__(self, discount_strategy: DiscountStrategy):
+        self._discount_strategy = discount_strategy
+
+    def apply_discount(self, product, items):
+        """
+            Applies discount :_discount_strategy:
+            to :product:
+        """
+        return self._discount_strategy.apply_discount(items, product.price_per_unit)
+
+
+# Concrete discount strategies
+
+class TwoPerOneDiscountStrategy(DiscountStrategy):
+    """ 2 per 1 Discount Strategy """
+    def apply_discount(self, items, price_per_item):
+        if items != 1:
+            # How many groups of two element (items // 2) plus rest
+            # Thats the number of 'items' times the price_per_item we have to charge
+            times = (items // 2) + (items % 2)
+            result = price_per_item * times
+        else:
+            result = price_per_item
+
+        return result
+
+
+class BulkDiscountStrategy(DiscountStrategy):
+    """ Bulk Discount Strategy """
+    def __init__(self, percent_discount, limit):
+        super(BulkDiscountStrategy, self).__init__()
+        self._percent_discount = percent_discount / float(100)
+        self._limit = limit
+
+    def apply_discount(self, items_number, price_per_item):
+        if items_number >= self._limit:
+            discounted_price = (1 - self._percent_discount) * price_per_item
+            result = discounted_price * items_number
+        else:
+            result = price_per_item * items_number
+
+        return result
---
+++ app/src/model/repository/discount_reader_repository.py
@@ -0,0 +1,42 @@
+from abc import ABC
+from abc import abstractmethod
+
+from src.db.db import DISCOUNTS
+from src.exceptions import DiscountNotFoundException
+from src.model.discount import Discount
+from src.model.discount import TwoPerOneDiscountStrategy
+from src.model.discount import BulkDiscountStrategy
+
+
+class DiscountReaderRepository(ABC):
+
+    @abstractmethod
+    def get(
+            self,
+            discount_code: str,
+            database: str = None,
+    ) -> Discount:
+        raise NotImplementedError()
+
+
+class DictionaryDiscountReaderRepository(DiscountReaderRepository):
+
+    def get(
+            self,
+            discount_code: str,
+            database: str = None,
+    ) -> Discount:
+
+        # Read discount information from DISCOUNTS dictionary
+        # "bulk25": {"description": "Bulk discount 25%", "status": "active", "percent": 25, "limit_items": 3}
+        if discount_code in DISCOUNTS:
+            # Here we decide the strategy we have to inject to Discount object
+            # to be applied. By code or type
+            if discount_code == '2per1':
+                discount_strategy = TwoPerOneDiscountStrategy()
+            elif DISCOUNTS[discount_code]['discount_type'] == 'BULK':
+                discount_strategy = BulkDiscountStrategy(DISCOUNTS[discount_code]['percent'],
+                                                         DISCOUNTS[discount_code]['limit_items'])
+            return Discount(discount_strategy)
+        else:
+            raise DiscountNotFoundException("Discount with code: {} not found".format(discount_code))
